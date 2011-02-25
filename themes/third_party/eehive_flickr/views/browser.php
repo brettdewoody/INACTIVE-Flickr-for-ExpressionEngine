a4f111d5c5f9cc495420b6182cdecd8a65cdd5a7
John D Wells <johndwells@gmail.com>
2011-02-25 19:46:56 +0000

    Remove redundant code.

--- themes/third_party/eehive_flickr/views/browser.php
+++ themes/third_party/eehive_flickr/views/browser.php
@@ -33,14 +33,6 @@
 	{
 		$photoBaseURL = "http://farm" . $photo['farm']. ".static.flickr.com/" . $photo['server'] . "/" . $photo['id'] . "_" . $photo['secret'];
 		$photoID = $photo['id'];
-		$photoUser = $photo['owner'];
-		$photoSize = "";
-		$photoTitle = $photo['title'];
-		$photoDescription = $photo['description'];
-		$photoArray = urlencode(serialize(array($photoBaseURL, $photoID, $photoSize, $photoTitle, $photoDescription, $photoUser)));
-
-		$photoBaseURL = "http://farm" . $photo['farm']. ".static.flickr.com/" . $photo['server'] . "/" . $photo['id'] . "_" . $photo['secret'];
-		$photoID = $photo['id'];
 		$photoSize = '';
 		$photoTitle = $photo['title'];
 		$photoDescription = $photo['description'];
